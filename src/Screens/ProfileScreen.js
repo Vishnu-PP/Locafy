32b0ca533bcbc767704eb6948f65cc57d9106689
vishnu.pp <vishnu.pp@acodez.co.in>
2024-12-24 18:25:27 +0530

    HomeScreen Changes

---
+++ src/Screens/ProfileScreen.js
@@ -0,0 +1,8 @@
+function ProfileScreen() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Profile Screen</Text>
+        </View>
+    );
+}
+export default ProfileScreen
